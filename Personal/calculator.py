fa8a994d47e62bbace6827c4a5332e8adbd74768
jinfluenza <jinwoov@gmail.com>
2020-08-28 10:32:10 -0700

    python 2

--- Personal/calculator.py
+++ Personal/calculator.py
@@ -2,7 +2,8 @@
 num1 = input("Enter a number: ")
 num2 = input("Enter a second number: ")
 
-# parsing the string to the number
-result = int(num1) + int(num2)
+# parsing the string to the number, if its decimal it will be error out
+#result = int(num1) + int(num2)
+result = float(num1) + float(num2)
 
 print(result)
