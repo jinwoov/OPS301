b927caf2c738427b13b31ff81a64c5bc85533a5d
jinfluenza <jinwoov@gmail.com>
2020-08-28 10:36:55 -0700

    python practice 2

--- Personal/calculator.py
+++ Personal/calculator.py
@@ -1,7 +1,7 @@
 
 num1 = input("Enter a number: ")
 num2 = input("Enter a second number: ")
-
+1
 # parsing the string to the number, if its decimal it will be error out
 #result = int(num1) + int(num2)
 result = float(num1) + float(num2)
---
+++ Personal/madlib.py
@@ -0,0 +1,9 @@
+# verbs noun and put that into a story
+
+color = input("Whats your favorite color? ")
+plural_noun = input("Enter plural noun: ")
+celebrity = input("Who's your favorite celebrity? ")
+
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)
