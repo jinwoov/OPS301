70335f6e8909049c88f97f73a00952e244f4eca1
jinfluenza <jinwoov@gmail.com>
2020-08-28 10:44:33 -0700

    python practice 3

---
+++ Personal/lists.py
@@ -0,0 +1,13 @@
+
+friends = ["mochi", "bori", "ally", "brandon", "max"]
+
+# to get after index 1
+print(friends[1:])
+
+# to get up to
+print(friends[1:3])
+
+# to modify
+friends[1] = "fox"
+
+print(friends[1])
