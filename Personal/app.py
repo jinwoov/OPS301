2ea3967b9be526d4ee3bb73263e6251b03966285
jinfluenza <jinwoov@gmail.com>
2020-08-27 23:55:23 -0700

    changes made on Thu 27 Aug 2020 11:55:23 PM PDT

---
+++ Personal/app.py
@@ -0,0 +1,39 @@
+# To create a variable
+character_name = "Jin"
+character_age = "99"
+
+print(character_name + " is " + character_age + " years old.")
+# reassigning the variable
+character_name = "mochi"
+print(character_name + " is " + character_age + " years old.")
+
+print("jin\nacademy")
+
+print(len(character_name))
+# to create a new line \n, to escape \
+
+# to create a upper case
+# string.upper()
+
+# to create a lower case
+# string.lower()
+
+# to check if its upper case
+# string.isupper()
+
+# len is a length of the string
+
+# chaining works for python
+
+# to get a specific character
+# index starts from 0
+my_character = "mochi"
+print(my_character[3])
+
+# to check what position the character is in
+print(my_character.index("h"))
+
+# to change the character 
+my_phrase = "animal kingdom"
+print(my_phrase.replace("animal", "vegetable"))
+
