af87122dbfe84c9173fd8fd7b1825cbba3e1fa4c
jinfluenza <jinwoov@gmail.com>
2020-08-28 11:07:56 -0700

    python function

---
+++ Personal/function.py
@@ -0,0 +1,10 @@
+
+# def is to create a function
+def hello_world(name):
+    # indentation is important to be one function
+    print("hello world " + name)
+
+# invoking the function
+name = input("whats your name?")
+hello_world(name)
+
