208808d46b543fc11ebe6282924f7bf7784029eb
jinfluenza <jinwoov@gmail.com>
2020-08-28 10:59:53 -0700

    python practice 5

---
+++ Personal/tuple.py
@@ -0,0 +1,10 @@
+
+# to create a tuples
+coordinates = (4, 5)
+
+# tuples is immutable. can't be changed or modified
+
+print(coordinates[0])
+
+# list is doing samething as tuple.
+# tuple is used for data that doesn't have to be changed.
