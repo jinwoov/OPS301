d388da150258081cda947305937c24c021c9429c
jinwoov <jinwoov@gmail.com>
2020-09-01 14:28:37 -0700

    done

---
+++ code-challenge/code-challenge02.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+# Script Name: Code Challenge 2
+# Author: Jin Kim
+# Date of last revision: 09/1/2020
+# Description of purpose: To create a bash script that copies /var/log/syslog to the current working directory and appends the current date and time to the filename.
+
+# Declaration of variables
+dateShort=`date -I`
+timeNowLocal=`date +%R`
+timestamp="$dateShort $timeNowLocal"
+fileName="$dateShort $timeNowLocal.txt"
+
+# Declaration of functions
+## Main function that creates the file
+CreatingFile() {
+cp "/var/log/syslog" "./"    
+}
+
+## Appending the date to the file
+Output() {
+    echo "$timestamp file before appending:"
+    ls ./
+    EmptySpace
+    mv "./syslog" "./syslog $fileName"
+    echo "$timestamp file after appending:"
+    ls ./
+}
+
+## HELPER METHODS ##
+## Created a mock statusbar that 
+ProgressBar() {
+    echo "$timestamp Creating a file with Date and time"
+    echo -ne '#####                   (33%)\r'
+    sleep 1
+    echo -ne '#############           (66%)\r'
+    sleep 1
+    echo -ne '####################### (100%)\r'
+    echo -ne '\n'
+}
+
+## funciton that creates empty space
+EmptySpace() {
+    echo
+}
+
+# Main (Invoking everything)
+ProgressBar
+EmptySpace
+CreatingFile
+Output
+
+# End
