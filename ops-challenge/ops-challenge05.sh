7070c700276cb5d66ccb9e863273401c7b82cd70
jinwoov <jinwoov@gmail.com>
2020-09-05 03:25:34 -0400

    finished the assignment

--- ops-challenge/ops-challenge05.sh
+++ ops-challenge/ops-challenge05.sh
@@ -10,7 +10,7 @@ MESSAGELOGS="/var/log/syslog"
 WTMP="/var/log/wtmp"
 
 # functions
-
+## This function use argument to find the filepath and erase the content
 function ClearContent()
 {
     > $1
@@ -43,6 +43,55 @@ ClearContent $WTMP
 
 ### Stretch Goals
 
+## linux log files
+echo "Following commands can be detrimental to your Linux system. YOU BEEN WARNED!"
+read
+
+## auth.log - authentication log
+AuthLog="/var/log/auth.log"
+read -p "Do you want to delete authentication log? (y|n)" Answer
+if [ $Answer -eq "y" ]
+then
+ClearContent $AuthLog
+fi
+
+## kern.log - information about kernel
+KernelLog="/var/log/kern.log"
+read -p "Do you want to delete kernel log? (y|n)" Answer
+if [ $Answer -eq "y" ]
+then
+ClearContent $KernelLog
+fi
+
+## boot.log - booting information
+BootLog="/var/log/boot.log"
+read -p "Do you want to booting log? (y|n)" Answer
+if [ $Answer -eq "y" ]
+then
+ClearContent $BootLog
+fi
+## http.log - information about web server apache
+HTTPLog="/var/log/HTTPLog"
+read -p "Do you want to web service log? (y|n)" Answer
+if [ $Answer -eq "y" ]
+then
+ClearContent HTTPLog
+fi
+
+## bash history: is a file that kept separate by user. 
+# cd /home/username/.bash_history
+HostName="hostname"
+BashHistory="/home/$HostName/.bash_history"
+read -p "Do you want to bash history? (y|n)" Answer
+if [ $Answer -eq "y" ]
+then
+ClearContent $BashHistory
+fi
+
+
+## Forensic tools
+## shred is delete the file permenantly
+## shred prevents recovery
 
 
 ### Clearing the log is important as to prevent any detection from forensic team. For the forensic team, they can use this data to analyze who and what kind of attack was done during the session. Attack leave malware for persistent connection, but that can lead a breadcrumbs for defensive to find where the attack is coming from.
