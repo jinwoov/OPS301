4bf0e0e960d133e699b57d2510b41bf68c16ca42
Jin Kim <jinwoov@gmail.com>
2020-09-06 21:27:58 -0700

    09/06/2020 21:27:58

---
+++ Personal/dsa/node.py
@@ -0,0 +1,43 @@
+# Node class
+class node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+    # printing the node
+    def PrintNode(self):
+        print(self.value)
+
+# linked list class
+class linkedlist:
+    def __init__(self):
+        self.head = None
+
+    def add(self, val):
+        newNode = node(val)
+        if self.head is None:
+            self.head = newNode
+        else:
+            current = self.head
+            while current.next is not None:
+                current = current.next
+            current.next = newNode
+    
+    def PrintLL(self):
+        if self.head is None:
+            raise IndexError("There is nothing in the linkedlist")
+        else:
+            current = self.head
+            while current is not None:
+                print (current.value)
+                current = current.next
+
+
+nodes = node(10)
+
+ll = linkedlist()
+ll.add(5)
+ll.add(5)
+ll.add(5)
+ll.add(5)
+ll.add(5)
+ll.PrintLL()
---
+++ ops-challenge/ops-challenges09.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+
+dictionary = {'Name': 'JIn', 'Age': 10, 'Race': 'Asian'}
+print ("dictionary['Name'] ", dictionary['Name'])
