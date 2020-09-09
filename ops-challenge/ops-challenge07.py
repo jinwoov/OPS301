f9082008bc7a0e57d2f1d35810fc9a8b90df3348
jinwoov <jinwoov@gmail.com>
2020-09-09 01:06:15 -0400

    all of the ops are done

--- ops-challenge/ops-challenge07.py
+++ ops-challenge/ops-challenge07.py
@@ -1,6 +1,6 @@
-# Script Name: Ops-Challenge 06
+# Script Name: Ops-Challenge 07
 # Author: Jin Kim
-# Date of last revision: 09/07/2020
+# Date of last revision: 09/08/2020
 # Description of purpose: This is to utitlize commands that's avaible in bash using python script file
 
 # bringing os library
@@ -8,11 +8,12 @@ import os
 
 
 # Declaration of variables
-
+## putting user's input into the variable
+userInput = input("what directory do you want to see? ")
 
 
 # Declaration of functions
-
+## taking the input from the user and displaying all of the files within user's input directory.
 def directory(inputs):
     for (root,dirs,files) in os.walk(inputs):
         print(root)
@@ -22,12 +23,14 @@ def directory(inputs):
         print(files)
         intoText(files)
 
+## putting all the out put to the text files
 def intoText(texts):
     fileWrite = open("./output.txt", 'a')
     fileWrite.writelines(texts)
     fileWrite.write("")
     fileWrite.close()
 
-
-userInput = input("what directory do you want to see? ")
+## main 
 directory(userInput)
+
+# END
