02ffd0662715c2708bcc915ca0a35bf829e715d6
jinwoov <jinwoov@gmail.com>
2020-09-09 01:50:38 -0400

    finished

--- ops-challenge/ops-challenge07.py
+++ ops-challenge/ops-challenge07.py
@@ -1,16 +1,14 @@
 # Script Name: Ops-Challenge 07
 # Author: Jin Kim
 # Date of last revision: 09/08/2020
-# Description of purpose: This is to utitlize commands that's avaible in bash using python script file
+# Description of purpose: This script file is to show what kind of files are in the directory 
 
 # bringing os library
 import os
 
-
 # Declaration of variables
 ## putting user's input into the variable
 userInput = input("what directory do you want to see? ")
-
 
 # Declaration of functions
 ## taking the input from the user and displaying all of the files within user's input directory.
@@ -23,14 +21,24 @@ def directory(inputs):
         print(files)
         intoText(files)
 
+
+## stretch goals
 ## putting all the out put to the text files
 def intoText(texts):
     fileWrite = open("./output.txt", 'a')
     fileWrite.writelines(texts)
     fileWrite.write("")
     fileWrite.close()
+## creating a user input directory and subdirectory of string
+def createDir():
+    userInput2 = input("What directory do you want to make? ")
+    os.makedirs(userInput2)
+    for i in range(1,4):
+        os.makedirs(f"{userInput2}/string_0{i}")
 
-## main 
+
+## Main 
 directory(userInput)
-
+os.system("libreoffice --writer ./output.txt")
+createDir()
 # END
