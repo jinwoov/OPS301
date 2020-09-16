e7d62aeb61f26046e0e4b5f3dd6f35791ce6f903
Jin Kim <jinwoov@gmail.com>
2020-09-16 14:41:20 -0700

    done

---
+++ classnotes/day13.md
@@ -0,0 +1,33 @@
+3 types of patches
+- features patch 
+- bug fix
+- security
+
+
+Treat the type differently
+- always pushed security patches
+- check with the update server
+
+- for bug fix and feature patch -> wait for them
+- can a security patches 
+- no continuity and no consistency 
+    - security red flag 
+- Server message block one was exploited 
+    - pet ya
+    - wernercris
+- SMB is not good and is still available
+- patch management : file that when you execute it changes the code to the operating system.
+- weakness or exposure to the system that cause exposure to the system.
+- vulneratbility management program: taking proactive steps to identify vulnerability
+    - identify and prevent, mitigate, classify
+- cybersecurity strategy -> vulnerability management -> Patch management (IT)
+                                                    -> Traffic filtering and scanning (VPN, firewall, traffic monitoring, ports, static routing)
+                                                    
+- how is your company defend proactively against this threat
+- Zero day vulnerability - day 0 ... 
+
+
+----
+LAb
+add role and features -> role base -> wsus -> check windows server update services
+
---
+++ ops-challenge/ops-challenge13.py
@@ -0,0 +1,138 @@
+#!/usr/bin/python
+import requests
+import getpass
+import os
+
+# Script Name: Ops Challenge: Class 12
+# Author: Jin Kim
+# Date of last revision: 09/15/2020
+# Description of purpose: To show different aspects of CPU information
+
+# Declaring variable
+
+
+# Declaring function
+def mainMenu():
+    userChoice = input(f"""
+    Hi {getpass.getuser()}! What do you want to perform?
+    1. GET
+    2. POST
+    3. PUT
+    4. DELETE
+    5. HEAD
+    6. PATCH
+    7. OPTIONS
+    8. Exit
+    Choice --> """)
+
+    if(userChoice == "1"):
+        getMethod()
+    elif(userChoice == "2"):
+        postMethod()
+    elif(userChoice == "3"):
+        putMethod()
+    elif(userChoice == "4"):
+        deleteMethod()
+    elif(userChoice == "5"):
+        headMethod()
+    elif(userChoice == "6"):
+        patchMethod()
+    elif(userChoice == "7"):
+        optionsMethod()
+    else:
+        exit(0)
+
+## Get function
+def getMethod():
+    if(validateChoice("GET")):
+        response = requests.get(url)
+        statCode = int(response.status_code)
+        statusCodes(statCode)
+
+## post function
+def postMethod():
+    if(validateChoice("POST")):
+        response = requests.post(url)
+        statCode = int(response.status_code)
+        statusCodes(statCode)
+
+## put function
+def putMethod():
+    if(validateChoice("PUT")):
+        response = requests.put(url)
+        statCode = int(response.status_code)
+        statusCodes(statCode)
+
+## Delete method
+def deleteMethod():
+    if(validateChoice("DELETE")):
+        response = requests.delete(url)
+        statCode = int(response.status_code)
+        statusCodes(statCode)
+
+## Head Method
+def headMethod():
+    if(validateChoice("HEAD")):
+        response = requests.head(url)
+        statCode = int(response.status_code)
+        statusCodes(statCode)
+
+## Patch Method
+def patchMethod():
+    if(validateChoice("PATCH")):
+        response = requests.patch(url)
+        statCode = int(response.status_code)
+        statusCodes(statCode)
+
+## Options Method
+def optionsMethod():
+    if(validateChoice("OPTIONS")):
+        response = requests.options(url)
+        statCode = int(response.status_code)
+        statusCodes(statCode)
+
+def statusCodes(statCode):
+    if (statCode >= 100 and statCode < 200):
+        print(colors.fg.orange,f"{getpass.getuser()}, you got informational response from {url}")
+    elif (statCode >= 200 and statCode < 300):
+        print(colors.fg.green,f"{getpass.getuser()}, your response came back successful from {url}")
+    elif (statCode >= 300 and statCode < 400):
+        print(colors.fg.orange,f"{getpass.getuser()}, you were redirected from {url}")
+    elif (statCode >= 400 and statCode < 500):
+        print(colors.fg.red,f"{getpass.getuser()}, you done mess up from {url}")
+    else:
+        print(colors.fg.red,f"{getpass.getuser()}, Server had error to your request from {url}")
+
+## Validating if the choice was yes
+def validateChoice(choice):
+    userOption = input(f"You chose to perform {choice} method to {url}. Do you want to proceed (y)es/(n)o? ")
+    if(userOption == "y" or userOption == "yes" or userOption == "Y" or userOption == "Yes"):
+        return True
+    else:
+        return False
+
+## what site does user want to query
+def whatSite():
+    url = input("which site do you want to request? ")
+    while (len(url) < 3):
+        print(colors.fg.red,"You entered wrong url", colors.reset)
+        url = input("which site do you want to request? ")
+    if(url.__contains__("https") == False):
+        url = f"https://{url}"
+    return url
+
+
+### HELPER FUNCTION
+## class that contains default color that can be used for console
+class colors:
+    reset='\033[0m'
+    class fg:
+        green='\033[32m'
+        red='\033[0;31m'
+        orange = '\033[33m'
+
+# MAIN
+url = whatSite()
+mainMenu()
+exit(0)
+# END
